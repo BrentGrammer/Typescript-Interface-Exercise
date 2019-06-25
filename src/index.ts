83ffdff4bd23925387f114d052bc657b45275c8d
Brent M <brentonmarquez@gmail.com>
2019-06-24 22:35:00 -0600

    add linkedlist to sortable classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, -5, 3]);
 const sorter = new Sorter(numbersCollection);
@@ -9,6 +10,15 @@ sorter.sort();
 const charactersCollection = new CharacterCollection('dcfgtza');
 const charsorter = new Sorter(charactersCollection);
 charsorter.sort();
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(3);
+linkedList.add(5);
+
+const llistSorter = new Sorter(linkedList);
+llistSorter.sort();
 
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
